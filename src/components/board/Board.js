2f5c6596eb5b230cbf5bfb3c338d3ecd0b322966
Avishai Taashur <avishai@avishai84.com>
2019-09-06 22:51:01 -0700

    components are talking to each other.

--- src/components/board/Board.js
+++ src/components/board/Board.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import Square from '../square/Square';
 
 function Board() {
   return (
     <div className="board">
- 
+         <p>Boared</p>
+        <Square />
     </div>
   );
 }
--- src/components/game/Game.js
+++ src/components/game/Game.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import Board from '../board/Board';
 
 function Game() {
   return (
     <div className="game">
- 
+         <p>Game</p>
+         <Board />
     </div>
   );
 }
